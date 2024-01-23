c942c85fdfcb907860dd7ad359465e5def15de4c
Nikita <poberezhnyuk.n.i@edu.mirea.ru>
2024-01-23 17:43:51 +0300

    v.1.0

---
+++ ClearDesktop.ps1
@@ -0,0 +1,23 @@
+﻿<#
+ClearDesktop Script v.1.0
+#>
+
+$date = Get-Date -Format "dd.MM.yyyy"
+
+$path = "C:\Users\katshido\Desktop\"
+
+$files = Get-ChildItem -File -Path $path
+
+if ($files.Count -ne 0)
+{
+    
+    if (!(Test-Path -Path ($path + $date)))
+    {
+        New-Item -Path $path -Name $date -ItemType Directory
+    }
+    
+    foreach ($file in $files)
+    {
+        Move-Item -Path ($path + $file.Name) -Destination ($path + $date + "\" + $file.Name)
+    }
+}
